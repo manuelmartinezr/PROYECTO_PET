dd0d5253f06f533d3f4eddf85092361e90671319
Manuel Alejandro Martínez Rincón <manuelmartinezr18@gmail.com>
2022-11-10 11:20:30 -0500

    renamed modules

---
+++ PP_folder/stats_bar.py
@@ -0,0 +1,10 @@
+class Stats_Bar:
+    def __init__(self) -> None:
+        self._hungry_stat = 250
+        self._bored_stat = 250
+
+    def update_hungry_stat(self, amount):
+        self._hungry_stat += amount
+
+    def update_bored_stat(self, amount):
+        self._bored_stat += amount
